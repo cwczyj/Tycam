7b46905b081f285ca99e06e61e42b882714e61dd
CWC <chenwanchun91@163.com>
2020-12-16 17:05:05 +0800

    First commit

---
+++ CMakeFiles/TYCam.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lipschitz/Documents/Code/C_Code/TYCam/main.cpp" "/home/lipschitz/Documents/Code/C_Code/TYCam/build/CMakeFiles/TYCam.dir/main.cpp.o"
+  "/home/lipschitz/Documents/Code/C_Code/TYCam/tycamCapture.cpp" "/home/lipschitz/Documents/Code/C_Code/TYCam/build/CMakeFiles/TYCam.dir/tycamCapture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "OPENCV_TRAITS_ENABLE_DEPRECATED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/lipschitz/Documents/Code/C_Code/TYCam/build/CMakeFiles/sample_common.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/TYCam.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TYCam.dir/main.cpp.o"
+  "CMakeFiles/TYCam.dir/tycamCapture.cpp.o"
+  "TYCam"
+  "TYCam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TYCam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
